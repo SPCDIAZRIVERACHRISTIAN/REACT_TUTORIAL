6384888d5d85f887286fe9eef7efa7e0f686ff21
Christian <christianda3@gmail.com>
2024-10-09 11:33:28 -0400

    Add initial project files and configurations

---
+++ my-app/src/App.js
@@ -0,0 +1,36 @@
+function Square({ value }) {
+    function handleCLick() {
+        console.log('Clicked!');
+    }
+
+    return (
+        <button
+        classname="square"
+        onClick={handleCLick}
+        >
+            {value}
+        </button>
+        );
+}
+
+export default function board() {
+    return (
+        <>
+        <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+    );
+  }
